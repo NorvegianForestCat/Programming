30d764e6261b4611f5a8daee5f544b5594be315a
alexey <alexey2005name@yandex.ru>
2025-03-19 19:50:33 +0700

    7 feature(Commands): Added Command pattern for loading and saving data

---
+++ src/Contacts/View/ModelView/LoadCommand.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+using View.Model;
+using View.Model.Services;
+
+namespace View.ModelView
+{
+    class LoadCommand : ICommand
+    {
+        private ContactSerializer _serializer { get; set; }
+        private MainVM _mainVM { get; set; }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public LoadCommand(MainVM viewmodel)
+        {
+            _mainVM = viewmodel;
+            _serializer = new ContactSerializer();
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            Contact loadedContact = _serializer.ContactLoad();
+
+            if (loadedContact != null)
+            {
+                _mainVM.Email = loadedContact.Email;
+                _mainVM.PhoneNumber = loadedContact.PhoneNumber;
+                _mainVM.Name = loadedContact.Name;
+            }
+        }
+    }
+}
---
+++ src/Contacts/View/ModelView/SaveCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Input;
+using View.Model;
+using View.Model.Services;
+
+namespace View.ModelView
+{
+    class SaveCommand : ICommand
+    {
+        private Contact _contact { get; set; }
+
+        private ContactSerializer _serializer { get; set; }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public SaveCommand(Contact contact)
+        {
+            _serializer = new ContactSerializer();
+            _contact = contact;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _serializer.ContactSave(_contact);
+        }
+    }
+}
