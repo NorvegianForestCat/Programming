26802f96f2b1b47ea39fa7471dca0abab1d76f1a
alexey <alexey2005name@yandex.ru>
2025-03-19 18:42:47 +0700

    6 feature(MainVM.cs): Added ViewModel for main window

---
+++ src/Contacts/View/ModelView/MainVM.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using View.Model;
+
+namespace View.ModelView
+{
+    /*
+    - предоставлять свойства Name, PhoneNumber и Email для привязки со стороны MainView; 
+    - экземпляр контакта Contact, в котором должна храниться вся актуальная информация с пользовательского интерфейса. 
+     */
+    class MainVM : INotifyPropertyChanged
+    {
+        public Contact Contact { get; set; }
+        public string Name
+        {
+            get => Contact.Name;
+            set
+            {
+                if (value != null)
+                {
+                    Contact.Name = value;
+                }
+            }
+        }
+        public string PhoneNumber
+        {
+            get => Contact.PhoneNumber;
+            set
+            {
+                if (value != null)
+                {
+                    Contact.PhoneNumber = value;
+                }
+            }
+        }
+        public string Email
+        {
+            get => Contact.Email;
+            set
+            {
+                if (value != null)
+                {
+                    Contact.Email = value;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainVM()
+        {
+            Contact = new Contact();
+        }
+    }
+}
