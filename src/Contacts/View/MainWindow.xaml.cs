4adcd9d26d42a442bcfc6d28dfa530d79deee656
alexey <alexey2005name@yandex.ru>
2025-03-19 19:53:19 +0700

    9 feature(MainWindow.xaml.cs): Added DataContext

--- src/Contacts/View/MainWindow.xaml.cs
+++ src/Contacts/View/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using View.ModelView;
 
 namespace View;
 
@@ -17,10 +9,12 @@ namespace View;
 public partial class MainWindow : Window
 {
     /// <summary>
-    /// 
+    /// Initializing main window object
     /// </summary>
     public MainWindow()
     {
         InitializeComponent();
+
+        DataContext = new MainVM();
     }
 }
